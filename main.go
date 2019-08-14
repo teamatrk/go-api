cdb9a67f329112d7712e3d6a4a011d672b585e80
Vijit Yadav <rkplanet.team@gmail.com>
2019-08-14 01:59:14 -0700

    second

--- main.go
+++ main.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
-	"app"
-	"db"
-	"gorilla/mux"
+	"fmt"
 )
 
 func main() {
-	database, err := db.CreateDatabase()
-	if err != nil {
-		log.Fatal("Database connection failed: %s", err.Error())
-	}
-
-	app := &app.App{
-		Router:   mux.NewRouter().StrictSlash(true),
-		Database: database,
-	}
-
-	app.SetupRouter()
-
-	log.Fatal(http.ListenAndServe(":8080", app.Router))
+	fmt.Println("hello")
 }
